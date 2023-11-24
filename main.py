95c2c20786916a252b41e39fca68658446fcf911
michaeldevon005@gmail.com <michaeldevon005@gmail.com>
2023-11-25 07:26:54 +0800

    update main.py

--- main.py
+++ main.py
@@ -71,7 +71,7 @@ with st.container():
     st.write("May you become a girl who obeys God words.")
     st.write("and May you also become a gorgeous adorable girl.")
 
-st.write("I also want to confess that I :heart: you, since years ago. I like your randomness, it's such a chaotic personality.")
+st.write("I also want to confess that I :heart: you, since years ago. I like your randomness, it's such a chaotic personality. hello")
 
 with open('style.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
